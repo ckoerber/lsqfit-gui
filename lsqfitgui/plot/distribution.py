7be63400cad02072cbd516f0cc9a1c5e17c6d630
Christopher Körber <software@ckoerber.com>
2021-09-24 10:58:21 +0200

    Extracted remaining functions from streamlit dashboard

---
+++ lsqfitgui/plot/distribution.py
@@ -0,0 +1,24 @@
+"""Plotting utility functions for distributions."""
+from typing import List
+
+import numpy as np
+from scipy.stats import norm
+
+import plotly.graph_objects as go
+
+
+def get_p2p_fig(fit) -> List[go.Figure]:
+    """Plot change of prior to posterior distribution."""
+    figs = {}
+    for n, (key, prior) in enumerate(fit.prior.items()):
+        posterior = fit.p[key]
+        fig = go.Figure(layout_title=key,)
+
+        for which, val in [("prior", prior), ("posterior", posterior)]:
+            x = np.linspace(val.mean - 3 * val.sdev, val.mean + 3 * val.sdev, 200)
+            y = norm(val.mean, val.sdev).pdf(x)
+            fig.add_trace(
+                go.Scatter(x=x, y=y, fill="tozeroy", name=which, showlegend=n == 0,),
+            )
+        figs[key] = fig
+    return figs
--- lsqfitgui/util/function.py
+++ lsqfitgui/util/function.py
@@ -1,6 +1,7 @@
 """Utilities for parsing and importing functions."""
-
+import re
 import importlib.util
+import sympy
 
 
 def parse_function(string: str):
@@ -15,3 +16,33 @@ def parse_function(string: str):
     module = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(module)
     return getattr(module, name)
+
+
+def parse_function_expression(fit) -> str:
+    """Parses the fit function and posteriror to latex label."""
+    expressions = {}
+    values = {}
+    for key, val in fit.p.items():
+        if hasattr(val, "__iter__"):
+            expr = sympy.symbols(" ".join([f"{key}{n}" for n, el in enumerate(val)]))
+        else:
+            expr = sympy.Symbol(key)
+
+        expressions[key] = expr
+
+        if hasattr(expr, "__iter__"):
+            for ee, vv in zip(expr, val):
+                values[sympy.latex(ee)] = vv
+        else:
+            values[sympy.latex(expr)] = val
+
+    try:
+        f_expr = fit.fcn(
+            x=sympy.Symbol("x"), p={key: expr for key, expr in expressions.items()}
+        )
+        s = sympy.latex(f_expr)
+        s = re.sub(r"\+\s+\-", "-", s)
+    except Exception:
+        s = None
+
+    return s
