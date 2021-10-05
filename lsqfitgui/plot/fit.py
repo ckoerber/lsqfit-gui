c0d3abad7b33b17d7baeb956a7b4d2fe503d012d
Christopher Koerber <software@ckoerber.com>
2021-10-05 10:40:52 +0200

    Only allowed log scale on single plot

--- lsqfitgui/plot/fit.py
+++ lsqfitgui/plot/fit.py
@@ -71,7 +71,7 @@ def plot_fit(fit, fig: Optional[Figure] = None):
         template="plotly_white",
         font={"size": 16},
         hoverlabel={"font_size": 16},
-        updatemenus=[LOG_MENU],
+        updatemenus=[LOG_MENU] if len(fit.y) == 1 else None,
     )
     return fig
 
