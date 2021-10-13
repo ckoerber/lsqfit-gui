3ddfea3bbbdf93cc16c9d54614b10bc1709b3caf
Christopher Körber <software@ckoerber.com>
2021-10-13 16:37:28 +0200

    Unified plotting API to use plot gvar

--- lsqfitgui/plot/fit.py
+++ lsqfitgui/plot/fit.py
@@ -6,136 +6,22 @@ import gvar as gv
 from plotly.graph_objects import Figure
 from plotly.subplots import make_subplots
 
-from lsqfitgui.plot.util import get_fit_bands, get_residuals
-from lsqfitgui.plot.uncertainty import plot_errors, plot_band
+from lsqfitgui.plot.util import get_residuals
+from lsqfitgui.plot.uncertainty import plot_gvar
 
 
-LOG_MENU = dict(
-    type="dropdown",
-    direction="down",
-    y=1.2,
-    x=0,
-    xanchor="left",
-    buttons=list(
-        [
-            dict(
-                args=[{"yaxis": {"type": "linear"}}],
-                label="Linear Scale",
-                method="relayout",
-            ),
-            dict(
-                args=[{"yaxis": {"type": "log"}}], label="Log Scale", method="relayout",
-            ),
-        ]
-    ),
-)
-
-
-def plot_fit(fit, fig: Optional[Figure] = None, fcn: Optional[Callable] = None, y = None): # add type hint
+def plot_fit(fit, fig: Optional[Figure] = None):  # add type hint
     """Plot data and fit error bands."""
-    x_fit, y_min_fit, y_mean_fit, y_max_fit = get_fit_bands(fit, fcn=fcn)
+    fig = plot_gvar(fit.x, fit.fcn(fit.x, fit.p), name="Fit", kind="band")
+    fig = plot_gvar(fit.x, fit.y, name="Data", kind="data", fig=fig, add_log_menu=True)
 
-    if not isinstance(fit.y, (dict, gv.BufferDict)):
-        fig = fig or Figure()
-
-        if y is None and fcn is None: # show y
-            plot_errors(fig, fit.x, gv.mean(fit.y), gv.sdev(fit.y), name="Data")
-        elif y is not None: # show transformed y
-            plot_errors(fig, fit.x, gv.mean(y), gv.sdev(y), name="Data")
-
-        if (
-            (fcn is not None) # fcn specified => show plot fcn
-            or (y is None) # y is None => using fit.y => show fit; 
-            or (y == fit.y).all() # y=fit.y as an argument => show fit
-        ):
-            plot_band(fig, x_fit, y_min_fit, y_mean_fit, y_max_fit, name="Fit")
-
-    else:
-        fig = fig or make_subplots(
-            cols=1, rows=len(fit.y), subplot_titles=list(fit.y.keys())
-        )
-
-        for n, (key, yy) in enumerate(fit.y.items()):
-            if y is None and fcn is None: # show y 
-                plot_errors(
-                    fig,
-                    fit.x[key] if isinstance(fit.x, dict) else fit.x,
-                    gv.mean(yy),
-                    gv.sdev(yy),
-                    name=f"{key} data",
-                    row=n + 1,
-                    col=1,
-                )
-            elif y is not None: # show transformed y
-                plot_errors(
-                    fig,
-                    fit.x[key] if isinstance(fit.x, dict) else fit.x,
-                    gv.mean(y[key]),
-                    gv.sdev(y[key]),
-                    name=f"{key} data",
-                    row=n + 1,
-                    col=1,
-                )
-
-            if (
-                (fcn is not None) # fcn specified => show plot fcn
-                or (y is None) # y is None => using fit.y => show fit; 
-                or all([(y[key] == fit.y[key]).all() for key in fit.y]) # y=fit.y as an argument => show fit
-            ):
-                plot_band(
-                    fig,
-                    x_fit[key] if isinstance(x_fit, dict) else x_fit,
-                    y_min_fit[key],
-                    y_mean_fit[key],
-                    y_max_fit[key],
-                    name=f"{key} fit",
-                    row=n + 1,
-                    col=1,
-                )
-
-        fig.update_layout(height=len(fit.y) * 300)
-
-    fig.update_layout(
-        template="plotly_white",
-        font={"size": 16},
-        hoverlabel={"font_size": 16},
-        updatemenus=[LOG_MENU]
-        if not isinstance(fit.y, (dict, gv.BufferDict))
-        else None,
-    )
     return fig
 
 
 def plot_residuals(fit, fig: Optional[Figure] = None):
     """Plot fit residuals."""
     residuals = get_residuals(fit)
-
-    if not isinstance(fit.y, (dict, gv.BufferDict)):
-        fig = fig or Figure()
-        plot_errors(fig, fit.x, gv.mean(residuals), gv.sdev(residuals))
-    else:
-        fig = fig or make_subplots(
-            cols=1, rows=len(fit.y), subplot_titles=list(fit.y.keys())
-        )
-        for n, (key, yy) in enumerate(fit.y.items()):
-            xx = fit.x[key] if isinstance(fit.x, dict) else fit.x
-            plot_errors(
-                fig,
-                xx,
-                gv.mean(residuals[key]),
-                gv.sdev(residuals[key]),
-                name=key,
-                row=n + 1,
-                col=1,
-            )
-
-        fig.update_layout(height=len(fit.y) * 300)
+    fig = plot_gvar(fit.x, residuals, name="Residuals", kind="data")
     fig.add_hline(0, line={"color": "black"})
 
-    fig.update_layout(
-        template="plotly_white",
-        font={"size": 16},
-        hoverlabel={"font_size": 16},
-        # updatemenus=[LOG_MENU],
-    )
     return fig
--- lsqfitgui/plot/uncertainty.py
+++ lsqfitgui/plot/uncertainty.py
@@ -5,6 +5,9 @@ import numpy as np
 import gvar as gv
 
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+from lsqfitgui.plot.util import LOG_MENU
 
 
 def wrap_plot_function(kind: str = "data", **wrapper_kwargs):
@@ -31,21 +34,67 @@ def plot_gvar(
     fig: Optional[go.Figure] = None,
     kind: str = "data",
     name: Optional[str] = None,
+    add_log_menu: bool = False,
     **kwargs,
 ):
     """Plots gvars."""
-    fig = fig or go.Figure()
-    mean = gv.mean(y)
-    sdev = gv.sdev(y)
+    fig_was_none = fig is None
 
-    if kind == "data":
-        plot_errors(fig, x, mean, sdev, name=name, scatter_kwargs=kwargs)
-    elif kind == "band":
-        plot_band(
-            fig, x, mean - sdev, mean, mean + sdev, name=name, scatter_kwargs=kwargs
-        )
+    if not isinstance(y, (dict, gv.BufferDict)):
+        fig = fig or go.Figure()
+        mean = gv.mean(y)
+        sdev = gv.sdev(y)
+
+        if kind == "data":
+            plot_errors(fig, x, mean, sdev, name=name, scatter_kwargs=kwargs)
+        elif kind == "band":
+            plot_band(
+                fig, x, mean - sdev, mean, mean + sdev, name=name, scatter_kwargs=kwargs
+            )
+        else:
+            raise KeyError(f"Does not know how to plot {kind}")
     else:
-        raise KeyError(f"Does not know how to plot {kind}")
+        fig = fig or make_subplots(cols=1, rows=len(y), subplot_titles=list(y.keys()))
+
+        for n, (key, yy) in enumerate(y.items()):
+            mean = gv.mean(y[key])
+            sdev = gv.sdev(y[key])
+            if kind == "data":
+                plot_errors(
+                    fig,
+                    x[key] if isinstance(x, dict) else x,
+                    mean,
+                    sdev,
+                    name=f"{name} {key} data",
+                    row=n + 1,
+                    scatter_kwargs=kwargs,
+                    col=1,
+                )
+            elif kind == "band":
+                plot_band(
+                    fig,
+                    x[key] if isinstance(x, dict) else x,
+                    mean - sdev,
+                    mean,
+                    mean + sdev,
+                    name=f"{name} {key} fit",
+                    scatter_kwargs=kwargs,
+                    row=n + 1,
+                    col=1,
+                )
+
+        if fig_was_none:
+            fig.update_layout(height=len(y) * 300)
+
+    if fig_was_none:
+        fig.update_layout(
+            template="plotly_white",
+            font={"size": 16},
+            hoverlabel={"font_size": 16},
+            updatemenus=None
+            if isinstance(y, (dict, gv.BufferDict)) or not add_log_menu
+            else [LOG_MENU],
+        )
 
     return fig
 
