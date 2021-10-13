37dfae5f21a605af36c7779f6a6bbb9724fddb64
Christopher Körber <software@ckoerber.com>
2021-10-13 16:41:20 +0200

    Update to re-include log menu

--- lsqfitgui/plot/fit.py
+++ lsqfitgui/plot/fit.py
@@ -1,10 +1,7 @@
 """Plotting routines associated with fit objects."""
-from typing import Callable, Optional
-
-import gvar as gv
+from typing import Optional
 
 from plotly.graph_objects import Figure
-from plotly.subplots import make_subplots
 
 from lsqfitgui.plot.util import get_residuals
 from lsqfitgui.plot.uncertainty import plot_gvar
@@ -12,8 +9,10 @@ from lsqfitgui.plot.uncertainty import plot_gvar
 
 def plot_fit(fit, fig: Optional[Figure] = None):  # add type hint
     """Plot data and fit error bands."""
-    fig = plot_gvar(fit.x, fit.fcn(fit.x, fit.p), name="Fit", kind="band")
-    fig = plot_gvar(fit.x, fit.y, name="Data", kind="data", fig=fig, add_log_menu=True)
+    fig = plot_gvar(
+        fit.x, fit.fcn(fit.x, fit.p), name="Fit", kind="band", add_log_menu=True
+    )
+    fig = plot_gvar(fit.x, fit.y, name="Data", kind="data", fig=fig)
 
     return fig
 
