390fa783b46c69b939be8005cbb64449111506c6
Christopher Koerber <software@ckoerber.com>
2021-10-28 14:01:12 +0200

    Updated plot functions to make more sense depending on fit.x values if available

--- lsqfitgui/plot/fit.py
+++ lsqfitgui/plot/fit.py
@@ -1,10 +1,11 @@
 """Plotting routines associated with fit objects."""
 from typing import Optional
 
+import gvar as gv
 from plotly.graph_objects import Figure
 
 from lsqfit import nonlinear_fit
-from lsqfit._extras import chained_nonlinear_fit
+from lsqfit._extras import chained_nonlinear_fit, unchained_nonlinear_fit
 
 from lsqfitgui.plot.util import get_residuals
 from lsqfitgui.plot.uncertainty import plot_gvar, interpolate
@@ -12,13 +13,22 @@ from lsqfitgui.plot.uncertainty import plot_gvar, interpolate
 
 def plot_fit(fit, fig: Optional[Figure] = None):  # add type hint
     """Plot data and fit error bands."""
-
     try:
         xx = interpolate(fit.x)
         yy = fit.fcn(xx, fit.p)
+
+        # Check that interpolated results make sense
+        if isinstance(xx, dict) and isinstance(yy, (dict, gv.BufferDict)):
+            for key, val in xx.items():
+                assert len(val) == len(yy[key])
+        elif isinstance(yy, (dict, gv.BufferDict)):
+            for key, val in yy.items():
+                assert len(val) == len(xx)
+        else:
+            assert len(xx) == len(yy)
     except Exception:
 
-        if isinstance(fit, chained_nonlinear_fit):
+        if isinstance(fit, (chained_nonlinear_fit, unchained_nonlinear_fit)):
             xx = None
             yy = fit.fcn(fit.p)
         elif isinstance(fit, nonlinear_fit):
