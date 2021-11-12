e8e697a04f48f6a8d86a5238e41145d0ef6e7eb9
Christopher Koerber <software@ckoerber.com>
2021-11-12 14:56:18 +0100

    Documented sidebar

--- lsqfitgui/frontend/sidebar.py
+++ lsqfitgui/frontend/sidebar.py
@@ -8,6 +8,7 @@ from dash.dependencies import ALL, Input, Output
 
 import dash_bootstrap_components as dbc
 
+from lsqfitgui.util.callback import CallbackWrapper
 from lsqfitgui.frontend.widgets.export_prior import (
     get_export_prior_widget,
     EXPORT_PRIOR_CALLBACK,
@@ -38,48 +39,69 @@ def get_float_widget(
 
 
 class Sidebar:
+    """Sidebar template class which creates forms for prior values and meta configs."""
 
     update_callback_inputs = [
         Input({"type": "prior", "name": ALL}, "id"),
         Input({"type": "prior", "name": ALL}, "value"),
         Input({"type": "meta", "name": ALL}, "value"),
     ]
+    """Callback input arguments when changing sidebar forms."""
 
     save_fit_callback_args = (
         Output("save-fit", "data"),
         [Input("save-fit-btn", "n_clicks")],
     )
+    """Callback arguments for saving a fit to file."""
 
-    export_prior_callback = EXPORT_PRIOR_CALLBACK
+    export_prior_callback: CallbackWrapper = EXPORT_PRIOR_CALLBACK
+    """Callback for exporting a prior to clipboard."""
 
-    style = {"overflow-y": "auto", "height": "100vh"}
-    className = "sticky-top bg-light p-4"
+    style: Dict[str, str] = {"overflow-y": "auto", "height": "100vh"}
+    """HTML style of sidebar."""
+    className: str = "sticky-top bg-light p-4"
+    """HTML class name of sidebar."""
 
     def __init__(
         self, meta_config: Optional[List[Dict[str, Any]]] = None,
     ):
-        """"""
+        """Initialize sidebar by setting private attributes.
+
+        Arguments:
+            meta_config: Configuration to setup meta forms for fit.
+        """
         self._meta: Dict[str, Any] = {}
         self._prior: Dict[str, GVar] = {}
         self._layout: Optional[html.Div] = None
         self._meta_config: List[Dict[str, Any]] = meta_config or []
 
     @property
-    def meta_config(self):
+    def meta_config(self) -> Optional[Dict]:
+        """Return current meta config."""
         return self._meta_config
 
     @property
+    def prior(self) -> Dict[str, GVar]:
+        """Return current prior."""
+        return self._prior
+
+    @property
     def layout(self) -> html.Div:
+        """Return current HTML layout.
+
+        This property is lazy loaded: it will call :meth:`Sidebar.get_sidebar` if not yet computed.
+        """
         if self._layout is None:
             self._layout = self.get_sidebar(self._prior, self._meta)
         return self._layout
 
     def update(self, prior: Dict[str, GVar], meta: Optional[Dict] = None) -> html.Div:
+        """Update the current layout with new prior and or meta values."""
         if meta:
             self._meta = meta
         if prior:
-            self.prior = prior
-        self._layout = self.get_sidebar(self.prior, self._meta)
+            self._prior = prior
+        self._layout = self.get_sidebar(self._prior, self._meta)
         return self._layout
 
     def get_sidebar(
